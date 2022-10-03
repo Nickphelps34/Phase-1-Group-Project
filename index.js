7d22bfdb3187d74780afa7a62810cb5fa08e6077
Adam Kabak <adamkabak@yahoo.com>
2022-10-03 15:52:22 -0400

    working on second submit

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ cityForm.addEventListener("submit", (e) => {
 
   if (Number(inputText.value)) {
     getWeatherByZip(inputText.value).then((data) => console.log(data));
-  } else {
+  } else if (!document.querySelector("#city-menu")) {
     getWeatherByCity(inputText.value).then((data) => {
       console.log(data);
+
       if (data.length > 1) {
         const dropDown = document.createElement("select");
         dropDown.id = "city-menu";
@@ -22,10 +23,46 @@ cityForm.addEventListener("submit", (e) => {
         });
       }
     });
+    //=======================  PROJECT PAUSE  ===================================
+
+    //  need to get final 'else' to call cityStateSubmit if possible. Otherwise need
+    // to figure out another way to call a second submit with different code on the
+    // same form element.
+  } else {
+    console.log("are you here?");
+    cityStateSubmit();
+  }
+  function cityStateSubmit() {
+    cityForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const dropDown = document.querySelector("#city-input");
+      console.log("worked!"), console.log("city-input: ", dropDown.value);
+    });
   }
 });
-
-function cityStateSubmit() {}
+//============================================================================
+//   if (Number(inputText.value)) {
+//     getWeatherByZip(inputText.value).then((data) => console.log(data));
+//   } else {
+//     getWeatherByCity(inputText.value)
+//       .then((data) => {
+//         console.log(data);
+//         if (data.length > 1) {
+//           const dropDown = document.createElement("select");
+//           dropDown.id = "city-menu";
+//           cityForm.append(dropDown);
+//           data.forEach((city) => {
+//             const option = document.createElement("option");
+//             option.textContent = city.state;
+//             dropDown.appendChild(option);
+//             //cityStateSubmit();
+//           });
+//         }
+//       })
+//       //.catch((e) => console.log("error", e));
+//   }
+// });
 
 const getWeatherByCity = (city) => {
   const apiByCity = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=ac4519ef112c6f4cc9a4b2e01e44fe76&units=imperial`;
